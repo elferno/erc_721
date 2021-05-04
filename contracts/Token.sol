eb939225e7760fe8248be0b0208c8ff8a6e227f8
elferno <elferno@inbox.ru>
2021-05-04 13:24:23 +0500

    v 1.0.0

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,11 +4,53 @@ pragma solidity >=0.7.0;
 contract Token
 {
 	// var
+	string constant public name = "DAPP Token";
+	string constant public symbol = "DAPT";
+	string constant public standard = "DAPP token v 1.0.0";
 	uint256 public totalSupply;
+	mapping (address => uint256) public balanceOf;
+
+	// events
+	event Transfer
+	(
+		address indexed _from,
+		address indexed _to	 ,
+		uint256 		_value
+	);
 
 	// construct
-	constructor ()
+	constructor (uint256 _initialSupply)
 	{
-		totalSupply = 1000000;
+		balanceOf [msg.sender] = _initialSupply;
+		totalSupply = _initialSupply;
+	}
+
+	// transfer
+	function transfer
+	(
+		address _to,
+		uint256 _value
+	)
+		public
+		returns (bool success)
+	{
+		require (balanceOf[msg.sender] >= _value, "you haven't enought tokens to commit this operation");
+		_transfer (msg.sender, _to, _value);
+		return true;
+	}
+
+	// private functions
+	function _transfer
+	(
+		address _from,
+		address _to,
+		uint256 _value
+	)
+		private
+	{
+		balanceOf [_from] -= _value;
+		balanceOf [_to  ] += _value;
+
+		emit Transfer (_from, _to, _value);
 	}
 }
--- test/Token.js
+++ test/Token.js
@@ -2,16 +2,59 @@ const Token = artifacts.require('Token')
 
 contract('Token', accounts =>
 {
+	// var
 	let contract
-	
+	const Await = {
+		TotalSupply	: 1000000,
+		Name		: 'DAPP Token',
+		Symbol		: 'DAPT',
+		Standard	: 'DAPP token v 1.0.0'
+	}
+	const Test = {
+		Transfer	: 100
+	}
+
+	// at test start
 	before(async() => {
 		contract = await Token.deployed()
 	})
 
-	it ('sets total supply upon deployment', async () =>
+	// tests
+	it ('initialize contract with correct values', async () =>
+	{
+		const name = await contract.name()
+		const symbol = await contract.symbol()
+		const standard = await contract.standard()
+		
+		assert.equal(name	 , Await.Name	 , 'sets the correct NAME')
+		assert.equal(symbol	 , Await.Symbol	 , 'sets the correct SYMBOL')
+		assert.equal(standard, Await.Standard, 'sets the correct STANDARD')
+	})
+
+	it ('allocates initial supply upon deployment', async () =>
 	{
 		const totalSupply = await contract.totalSupply()
+		const balanceOf = await contract.balanceOf(accounts[0])
 		
-		assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1.000.000')
+		assert.equal(totalSupply.toNumber(), Await.TotalSupply, `total supply = ${Await.TotalSupply}`)
+		assert.equal(balanceOf.toNumber(), Await.TotalSupply,`creator balance = ${Await.TotalSupply}`)
+	})
+
+	it ('transfers token ownership', async () =>
+	{
+		const receipt = await contract.transfer(accounts[1], Test.Transfer, { from: accounts[0] })
+		const balanceOf_0 	 = await contract.balanceOf(accounts[0])
+		const balanceOf_1 	 = await contract.balanceOf(accounts[1])
+		const afterBalance_0 = Await.TotalSupply - Test.Transfer
+		const afterBalance_1 = Test.Transfer
+
+		assert.equal(balanceOf_0.toNumber()		 , afterBalance_0, `balance subtracted`)
+		assert.equal(balanceOf_1.toNumber()		 , afterBalance_1, `balance added`)
+
+		assert.equal(receipt.logs.length		 , 1			   , `triggers one event`)
+		assert.equal(receipt.logs[0].event		 , 'Transfer'	   , `should be the "Transfer" event`)
+		assert.equal(receipt.logs[0].args._from  , accounts[0]	   , `account FROM`)
+		assert.equal(receipt.logs[0].args._to 	 , accounts[1]	   , `account TO`)
+		assert.equal(receipt.logs[0].args._value , Test.Transfer   , `tokens transfered`)
 	})
 })
