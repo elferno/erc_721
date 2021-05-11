e7a35a6183321cfd6ae6c5a6fc1e20a425e35480
elferno <elferno@inbox.ru>
2021-05-11 09:30:16 +0500

    v 1.2.0

--- #src/js/app.js
+++ #src/js/app.js
@@ -12,6 +12,10 @@ const App = {
 	init: async function ()
 	{
 		await this.init_WEB_3()
+		if (this.account === undefined) {
+			this.request_WALLET()
+			return
+		}
 		await this.init_CONTRACT()
 		await this.render()
 	},
@@ -28,23 +32,22 @@ const App = {
 
 		[this.account] = await this.web3.eth.getAccounts()
 
-		if (this.account === undefined) {
-			// request metamask to plug on some accounts
+		// request metamask to plug on some accounts
+		if (this.account === undefined)
 			this.web3.eth.requestAccounts()
-		} else {
-			// conversion functions
-			const that = this
 
-			const hex_f = function( ) { return that.web3.utils.toHex(this.valueOf()) }
-			const wei_f = function(u) { return that.web3.utils.toWei(this.valueOf().toString(), u) }
-			const eth_f = function(u) { return that.web3.utils.fromWei(this.valueOf().toString(), u) }
-			const poi_f = function( ) { return this.valueOf().toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.") }
+		// conversion functions
+		const that = this
 
-			Number.prototype.toETH = eth_f; String.prototype.toETH = eth_f
-			Number.prototype.toWEI = wei_f;	String.prototype.toWEI = wei_f
-			Number.prototype.toHEX = hex_f;	String.prototype.toHEX = hex_f
-			Number.prototype.toPOI = poi_f;	String.prototype.toPOI = poi_f
-		}
+		const hex_f = function( ) { return that.web3.utils.toHex(this.valueOf()) }
+		const wei_f = function(u) { return that.web3.utils.toWei(this.valueOf().toString(), u) }
+		const eth_f = function(u) { return that.web3.utils.fromWei(this.valueOf().toString(), u) }
+		const poi_f = function( ) { return this.valueOf().toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.") }
+
+		Number.prototype.toETH = eth_f; String.prototype.toETH = eth_f
+		Number.prototype.toWEI = wei_f;	String.prototype.toWEI = wei_f
+		Number.prototype.toHEX = hex_f;	String.prototype.toHEX = hex_f
+		Number.prototype.toPOI = poi_f;	String.prototype.toPOI = poi_f
 	},
 
 
@@ -63,27 +66,44 @@ const App = {
 	},
 
 
+	// request wallet if it is not connected to website
+	request_WALLET: function ()
+	{
+		this.id('.main-content').empty()
+		this.id('.main-notice').html('Please require your ETH wallet and than reload the page')
+	},
+
+
 	// render the page
 	render: async function ()
 	{
 		// var
 		//await this.contracts.Token.methods.transfer(this.contracts.TokenSale.options.address, 750000).send({ from: this.account })
 
-		const _tokenBalance = await this.contracts.Token.methods.balanceOf(this.account).call()
+		const [_tokenBalance] = await this.handle(this.contracts.Token.methods.balanceOf(this.account).call())
 
 		const _TokenSellAddress = this.contracts.TokenSale.options.address
-		const _price 	  = await this.contracts.TokenSale.methods.token_Price().call()
-		const _soldTokens = await this.contracts.TokenSale.methods.token_Sold().call()
-		const _leftTokens = await this.contracts.Token.methods.balanceOf(_TokenSellAddress).call()
+		const [_price] = await this.handle(this.contracts.TokenSale.methods.token_Price().call())
+		const [_soldTokens] = await this.handle(this.contracts.TokenSale.methods.token_Sold().call())
+		const [_leftTokens] = await this.handle(this.contracts.Token.methods.balanceOf(_TokenSellAddress).call())
 
-		// render
-		this.id('.your-account').html(this.account)
+		if (_tokenBalance === null) {
+			// wallet reqired but not connected to the correct network
+			const notice = this.id('.notice')
+			notice.html(`<b>NOTICE:</b> Your wallet is not connected to the correct network. Please connect it to <b>HTTP://localhost:7545</b> and reload page`)
+			notice.classList.add('error')
+		} else {
+			// normal page render
+			this.id('.your-account').html(this.account)
 
-		this.id('.token-price').html(_price.toETH())
-		this.id('.token-amount').html(_tokenBalance.toPOI())
+			this.id('.token-price').html(_price.toETH())
+			this.id('.token-amount').html(_tokenBalance.toPOI())
 
-		this.id('.token-sold').html(_soldTokens.toPOI())
-		this.id('.token-total').html((+_leftTokens + +_soldTokens).toPOI())
+			this.id('.token-sold').html(_soldTokens.toPOI())
+			this.id('.token-total').html((+_leftTokens + +_soldTokens).toPOI())
+		}
+
+		this.id('.main-content').css('opacity: 1;')
 	},
 
 
@@ -110,13 +130,13 @@ const App = {
 		{
 			const el = elem[i]
 
-			el.style 	= (s) => { if (s) el.setAttribute('style', s); else el.removeAttribute('style') }
-			el.html 	= (h) => { el.innerHTML = h }
-			el.hide 	= ()  => { el.style.display = 'none' }
-			el.show 	= ()  => { el.style.display = '' }
-			el.val 		= ()  => { return el.value }
-			el.empty	= ()  => { while (el.firstChild) el.removeChild(el.firstChild) }
-			el.append	= (h) => { el.insertAdjacentHTML('beforeend', h); }
+			el.css	  = (s) => { if (s) el.setAttribute('style', s); else el.removeAttribute('style') }
+			el.html   = (h) => { el.innerHTML = h }
+			el.hide   = ()  => { el.style.display = 'none' }
+			el.show   = ()  => { el.style.display = '' }
+			el.val 	  = ()  => { return el.value }
+			el.empty  = ()  => { while (el.firstChild) el.removeChild(el.firstChild) }
+			el.append = (h) => { el.insertAdjacentHTML('beforeend', h); }
 		}
 
 		return elem.length === 1 ? elem[0] : elem;
